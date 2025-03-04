b4b6bdf3b3d4a42606f3a6e27c091028b0a763a3
Emrah Kacar <151836612+emrahkacar@users.noreply.github.com>
2025-03-04 19:49:56 +0000

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -10,7 +10,7 @@ argn = arg_needle_lib.deserialize_arg("/mnt/project/data/ARGs/arg_needle_example
 # Convert to tskit format
 arg_tree = arg_needle_lib.arg_to_tskit(arg = argn)
 
-# A larger tree, on a log timescale
+# Plot a larger tree including all generations
 wide_fmt = (20000, 300) 
 node_label_style = (
     ".node > .lab {font-size: 50%}"  # Reduce font size further
@@ -18,10 +18,11 @@ node_label_style = (
 )
 
 arg_tree.first().draw_svg(
+    path="tskit_tree_example_array_plot.svg",
     size=wide_fmt,
     time_scale="rank",
     y_gridlines=True,
     y_axis=True,
-    y_ticks=[1, 10, 100, 1000],
+    y_ticks=[1, 50, 100, 1000],
     style=node_label_style,
 )
