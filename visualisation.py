ada00d854aeba9ddb6a96a6d8f2ce89efc3deb6a
Emrah Kacar <151836612+emrahkacar@users.noreply.github.com>
2025-03-04 19:28:36 +0000

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -11,14 +11,15 @@ argn = arg_needle_lib.deserialize_arg("/mnt/project/data/ARGs/arg_needle_example
 arg_tree = arg_needle_lib.arg_to_tskit(arg = argn)
 
 # A larger tree, on a log timescale
-wide_fmt = (7000, 250)
+wide_fmt = (20000, 300) 
 node_label_style = (
-    ".node > .lab {font-size: 80%}"
-    ".leaf > .lab {text-anchor: start; transform: rotate(90deg) translate(6px)}"
+    ".node > .lab {font-size: 50%}"  # Reduce font size further
+    ".leaf > .lab {text-anchor: start; transform: rotate(90deg) translate(8px)}"
 )
+
 arg_tree.first().draw_svg(
     size=wide_fmt,
-    time_scale="log_time",
+    time_scale="rank",
     y_gridlines=True,
     y_axis=True,
     y_ticks=[1, 10, 100, 1000],
