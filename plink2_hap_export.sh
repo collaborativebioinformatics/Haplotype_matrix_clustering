0687c5bef1c0da11bc19dcde15378f8d833c7b27
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-04 03:04:35 -0500

    Update plink2_hap_export.sh

--- plink2_hap_export.sh
+++ plink2_hap_export.sh
@@ -1,4 +1,5 @@
 # keeping track of plink commands used to export haplotypes from SHAPEIT2 phased 1KG VCF input per chromosome
+# exclude multiallelic positions (--max-alleles 2)
 # run in DNAnexus ttyd, default parameters
 # chromosome 21
 plink2 --vcf /mnt/project/data/vcf/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz --export haps --out ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes --max-alleles 2
