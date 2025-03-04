d09f41622193912651f57a1c08adb2c27acfff96
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-04 13:12:30 -0500

    Update arg_needle_per_chromosome.sh

--- arg_needle_per_chromosome.sh
+++ arg_needle_per_chromosome.sh
@@ -1,4 +1,9 @@
 #run within dxjupyterlab console
+# run example for Emrah
+arg_needle --hap_gz example.hap.gz \
+--map example.map \
+--out example \
+--mode array
 # chromosome 6
 arg_needle --hap_gz /mnt/project/data/hap/ALL.chr6.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.haps \
 --map /mnt/project/data/ARGs/genetic_map_hg19.txt \
