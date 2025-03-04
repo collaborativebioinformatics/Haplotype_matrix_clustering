6324a293830c99f606ad3607fb6b293675eaed44
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-04 14:00:51 -0500

    Update plink2_hap_export.sh

--- plink2_hap_export.sh
+++ plink2_hap_export.sh
@@ -45,5 +45,21 @@ plink_hap_reformat_dx_upload "chr6"
 plink_hap_reformat_dx_upload "chr8"
 plink_hap_reformat_dx_upload "chr21"
 plink_hap_reformat_dx_upload "chr22"
-
-
+# redo with SNPs only!!
+function plink_hap_snp_reformat_dx_upload () {
+# note some files are VCF and some are gzipped VCF (either one or other, not both)
+# plink hap extraction with variant relabeling (no missing variant IDs)
+plink2 --vcf $(ls /mnt/project/data/vcf/ALL.${test}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf* | head -n 1) --new-id-max-allele-len 280 --set-missing-var-ids @:#[b37]\$r,\$a --export haps --out ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps --max-alleles 2
+# samples file reformatting
+paste <(cut -f2 /mnt/project/data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.sample -d' '| sed 's/ID_2/ID_1/g') <(cut -f2-4 /mnt/project/data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.sample -d ' ') -d ' ' > ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.sample
+# push back to /data/hap
+dx upload ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.* --path /data/hap
+# clean up leftover garbage
+rm -rf ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes*
+}
+# run on each chromosome
+# run on each chromosome
+plink_hap_snp_reformat_dx_upload "chr6"
+plink_hap_snp_reformat_dx_upload "chr8"
+plink_hap_snp_reformat_dx_upload "chr21"
+plink_hap_snp_reformat_dx_upload "chr22"
