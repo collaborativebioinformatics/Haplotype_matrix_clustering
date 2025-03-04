8ae74720029b3b04693e5a3b768e103a18eef1f5
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-04 12:19:17 -0500

    Create arg_needle_per_chromosome.sh
    Initial arg needle parameters

---
+++ arg_needle_per_chromosome.sh
@@ -0,0 +1,6 @@
+#run within dxjupyterlab console
+arg_needle --hap_gz /mnt/project/data/hap/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.haps \
+--map /mnt/project/data/ARGs/genetic_map_hg19.txt \
+--out /mnt/project/data/ARGs/chr21 \
+--mode sequence \
+--chromosome 21
