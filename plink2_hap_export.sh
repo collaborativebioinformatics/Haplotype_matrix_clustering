54b5ff53b6239d701aafc1811b0eb595eabc07b9
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-04 03:02:46 -0500

    Update plink2_hap_export.sh

--- plink2_hap_export.sh
+++ plink2_hap_export.sh
@@ -6,4 +6,12 @@ dx upload ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes* -
 # chromosome 22
 plink2 --vcf /mnt/project/data/vcf/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz --export haps --out ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes --max-alleles 2
 dx upload ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes* --path /data/hap
-# make chromosome variable/array to expedite next steps
+# possibly make chromosome variable/array to expedite next steps
+# main subroutine to reproduce steps above (export haplotypes from VCF, upload to /data/hap via dx)
+function plink_hap_dx_upload () {
+# note some files are VCF and some are gzipped VCF (either one or other, not both)
+plink2 --vcf /mnt/project/data/vcf/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf* --export haps --out ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes --max-alleles 2
+dx upload ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes* --path /data/hap
+}
+plink_hap_dx_upload "chr6"
+plink_hap_dx_upload "chr8"
