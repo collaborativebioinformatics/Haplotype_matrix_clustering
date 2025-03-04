c31b3b2f8c1b6425b7ebd6ae21e92ede9ba9b8d0
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-04 14:37:51 -0500

    Update arg_needle_per_chromosome.sh

--- arg_needle_per_chromosome.sh
+++ arg_needle_per_chromosome.sh
@@ -15,13 +15,13 @@ arg_needle --asmc_clust 1 --out example_seq_clust
 # chromosome 6
 arg_needle --hap_gz /mnt/project/data/hap/ALL.chr6.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.haps \
 --map /mnt/project/data/ARGs/genetic_map_hg19.txt \
---out /mnt/project/data/ARGs/chr6 \
+--out chr6 \
 --mode sequence \
 --chromosome 6
 # chromosome 21
 arg_needle --hap_gz /mnt/project/data/hap/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.haps \
 --map /mnt/project/data/ARGs/genetic_map_hg19.txt \
---out /mnt/project/data/ARGs/chr21 \
+--out chr21 \
 --mode sequence \
 --chromosome 21
 
