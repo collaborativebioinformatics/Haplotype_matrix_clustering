d94cc1f4ff9526672186f01ffab9a2701881776c
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-04 11:57:39 -0500

    Update plink2_hap_export.sh

--- plink2_hap_export.sh
+++ plink2_hap_export.sh
@@ -18,3 +18,29 @@ rm -rf ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes*
 }
 plink_hap_dx_upload "chr6"
 plink_hap_dx_upload "chr8"
+# prepare sample files in a form amenable to ARG-needle (ID_1 and ID_2 match)
+# must be space delimited
+function reformat_hap_sample () {
+paste <(cut -f2 /mnt/project/data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.sample -d' '| sed 's/ID_2/ID_1/g') <(cut -f2-4 /mnt/project/data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.sample -d ' ') -d ' ' > ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.sample
+}
+# run on all four chromosomes
+reformat_hap_sample "chr6"
+reformat_hap_sample "chr8"
+reformat_hap_sample "chr21"
+reformat_hap_sample "chr22"
+# redo everything with unique variant IDs after troubles with arg_needle
+function plink_hap_reformat_dx_upload () {
+# note some files are VCF and some are gzipped VCF (either one or other, not both)
+plink2 --vcf /mnt/project/data/vcf/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf* --new-id-max-allele-len 280 --set-missing-var-ids @:#[b37]\$r,\$a --export haps --out ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes --max-alleles 2
+paste <(cut -f2 /mnt/project/data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.sample -d' '| sed 's/ID_2/ID_1/g') <(cut -f2-4 /mnt/project/data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.sample -d ' ') -d ' ' > ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.sample
+dx upload ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes* --path /data/hap
+# clean up leftover garbage
+rm -rf ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes*
+}
+# run on each chromosome
+plink_hap_reformat_dx_upload "chr6"
+plink_hap_reformat_dx_upload "chr8"
+plink_hap_reformat_dx_upload "chr21"
+plink_hap_reformat_dx_upload "chr22"
+
+
