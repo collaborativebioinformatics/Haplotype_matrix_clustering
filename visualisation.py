dc5e9e001e2b4c5bfcea94b8895dcda2a36f80d6
Emrah Kacar <151836612+emrahkacar@users.noreply.github.com>
2025-03-04 18:42:44 +0000

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -1,1 +1,13 @@
+# Load libraries 
+# pip install tskit
+# pip install arg_needle
+import tskit
+import arg_needle_lib
+
+# Load arg file
+argn = arg_needle_lib.deserialize_arg("/mnt/project/data/ARGs/arg_needle_example/example.argn")
+
+# Convert to tskit format
+arg_tree = arg_needle_lib.arg_to_tskit(arg = argn)
+
 
