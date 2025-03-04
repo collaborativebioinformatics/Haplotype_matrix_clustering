9c660224c6dea0a3b19fa52328686317a1e59f2c
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-04 13:21:21 -0500

    Update arg_needle_per_chromosome.sh

--- arg_needle_per_chromosome.sh
+++ arg_needle_per_chromosome.sh
@@ -10,8 +10,8 @@ arg_needle
 arg_needle --asmc_clust 1
 # then sequence
 arg_needle_prepare_example --mode sequence --length 5e5
-arg_needle
-arg_needle --asmc_clust 1 
+arg_needle --out example_seq
+arg_needle --asmc_clust 1 --out example_seq_clust
 # chromosome 6
 arg_needle --hap_gz /mnt/project/data/hap/ALL.chr6.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.haps \
 --map /mnt/project/data/ARGs/genetic_map_hg19.txt \
