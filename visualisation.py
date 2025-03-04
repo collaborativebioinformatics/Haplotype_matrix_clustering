2d400f24a27c4a6c43e17edaf618752e9b312d77
Emrah Kacar <151836612+emrahkacar@users.noreply.github.com>
2025-03-04 20:13:44 +0000

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -11,12 +11,17 @@ argn = arg_needle_lib.deserialize_arg("/mnt/project/data/ARGs/arg_needle_example
 arg_tree = arg_needle_lib.arg_to_tskit(arg = argn)
 
 # Plot a larger tree including all generations
+
+# 'wide_fmt' defines the size of the plot (width, height) in pixels
 wide_fmt = (20000, 300) 
+
+# This node label style reduces font size for node labels and rotates the leaf labels for better overall visibility
 node_label_style = (
-    ".node > .lab {font-size: 50%}"  # Reduce font size further
+    ".node > .lab {font-size: 50%}"  # Reduce font size 
     ".leaf > .lab {text-anchor: start; transform: rotate(90deg) translate(8px)}"
 )
 
+# Plot first tree with draw_svg
 arg_tree.first().draw_svg(
     path="tskit_tree_example_array_plot.svg",
     size=wide_fmt,
