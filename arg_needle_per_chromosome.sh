4df13b191d5910eebdd3c62e56d5ca2fba4d3fd8
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-05 00:46:18 -0500

    Update arg_needle_per_chromosome.sh

--- arg_needle_per_chromosome.sh
+++ arg_needle_per_chromosome.sh
@@ -28,7 +28,7 @@ arg_needle --hap_gz /mnt/project/data/hap/ALL.chr21.phase3_shapeit2_mvncall_inte
 paste <(cut -f1,2 -d' ' ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.haps) <(yes "0" | head -n1054447) <(cut -f3 -d' ' ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.haps)
 # see prepare_genetic_hap_maps.sh for map file correction
 run_arg_needle_wrapper () {
-arg_needle --hap_gz /mnt/project/data/hap/ALL.chr${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.haps \
+arg_needle --hap_gz /mnt/project/data/hap/ALL.chr${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.haps \
 --map /mnt/project/data/hap/ALL.chr${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.map \
 --out chr${1} \
 --mode sequence \
