5978b6ef21daf138a5c8c37f12fe8622827123c5
Emrah Kacar <151836612+emrahkacar@users.noreply.github.com>
2025-03-05 11:03:35 +0000

    Update opencravat_snp_annotation.sh

--- opencravat_snp_annotation.sh
+++ opencravat_snp_annotation.sh
@@ -1,5 +1,8 @@
 # Download open-cravat for annotation
 # conda install open-cravat or pip install open-cravat
 
-# Filter vcf file to biallelic SNPs
+# Filter only biallelic SNPs
 bcftools view -v snps -m2 -M2 -O v -o ALL.chr6.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes_snps.vcf /mnt/project/data/vcf/ALL.chr6.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf
+
+# Run annotation for build hg19
+oc run ALL.chr6.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes_snps.vcf -d . -l hg19
