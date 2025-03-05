bc77aab3da9b5f91db9b5bedca2d8cedb45b3de3
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-05 01:12:58 -0500

    Update prepare_genetic_hap_maps.sh

--- prepare_genetic_hap_maps.sh
+++ prepare_genetic_hap_maps.sh
@@ -3,13 +3,15 @@ plink2 --haps ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotype
 # Eagle_v2.4.1/eagle --geneticMapFile /mnt/project/data/ARGs/genetic_map_hg19.txt --bfile ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps --outPrefix ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps --numThreads 60
 # didn't work, don't use Eagle
 # do use this
-conda create -n igp -c https://raw.githubusercontent.com/lightning-auriga/conda-builds/default/conda-builds -c bioconda -c conda-forge interpolate-genetic-position
+
+/home/dnanexus/miniforge3/bin/mamba create -n igp -c https://raw.githubusercontent.com/lightning-auriga/conda-builds/default/conda-builds -c bioconda -c conda-forge interpolate-genetic-position
 conda activate igp
 # make new map file with interpolated genetic positions
 interpolate-genetic-position.out -i ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.bim -p bim -g /mnt/project/data/ARGs/genetic_map_hg19.txt -m bolt -o ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.interpolated.map -f map
 # prep hap map function
 prep_hap_maps () {
-plink2 --haps ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.haps --out ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps --make-bed
+# keep only first of duplicated positions so that map is monotonically increasing
+plink2 --haps /mnt/project/data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.haps --rm-dups force-first --out ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps --make-bed
 interpolate-genetic-position.out -i ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.bim -p bim -g /mnt/project/data/ARGs/genetic_map_hg19.txt -m bolt -o ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.interpolated.map -f map
 dx upload ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.interpolated.map --path /data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.map
 }
