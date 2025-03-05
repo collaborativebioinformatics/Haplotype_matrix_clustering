0dd3b99416f52c2d980c460f03939edd8be6780c
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-05 12:06:16 -0500

    Update filter_hap_for_genes.sh

--- filter_hap_for_genes.sh
+++ filter_hap_for_genes.sh
@@ -3,6 +3,6 @@
 # HLAA - chr 6 29910309..29913647 on hg37 per https://www.ncbi.nlm.nih.gov/gene/3105
 # DEFB1 defensin beta 1 - chr 8 6728114..6735458 on hg37 per https://www.ncbi.nlm.nih.gov/gene/1672
 plink2 ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.*
-dx upload TNF.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap --path /data/haps/genes/TNF.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap
-dx upload HLAA.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap --path /data/haps/genes/HLAA.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap
-dx upload DEFB1.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap --path /data/haps/genes/DEFB1.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap
+dx upload TNF.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap --path /data/hap/genes/TNF.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap
+dx upload HLAA.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap --path /data/hap/genes/HLAA.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap
+dx upload DEFB1.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap --path /data/hap/genes/DEFB1.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.dedup_snps.hap
