2acc8a9bf1dbb37ca744fd01829c36d513842739
Abraham (Jon) Moller <molleraj@gmail.com>
2025-03-05 00:23:15 -0500

    Update prepare_genetic_hap_maps.sh

--- prepare_genetic_hap_maps.sh
+++ prepare_genetic_hap_maps.sh
@@ -7,3 +7,14 @@ conda create -n igp -c https://raw.githubusercontent.com/lightning-auriga/conda-
 conda activate igp
 # make new map file with interpolated genetic positions
 interpolate-genetic-position.out -i ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.bim -p bim -g /mnt/project/data/ARGs/genetic_map_hg19.txt -m bolt -o ALL.chr21.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.interpolated.map -f map
+# prep hap map function
+prep_hap_maps () {
+plink2 --haps ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.haps --out ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps --make-bed
+interpolate-genetic-position.out -i ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.bim -p bim -g /mnt/project/data/ARGs/genetic_map_hg19.txt -m bolt -o ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.interpolated.map -f map
+dx upload ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.interpolated.map --path /data/hap/ALL.${1}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.snps.map
+}
+# if necessary
+conda activate igp
+prep_hap_maps "chr6"
+prep_hap_maps "chr8"
+prep_hap_maps "chr22"
